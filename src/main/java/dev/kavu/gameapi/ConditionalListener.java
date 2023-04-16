49a2d86e7412b40be0306434a81cd3b6f147a981
TheKaVu <k.wieczaszek@gmail.com>
2023-04-16 20:58:50 +0200

    Added ConditionalListener.java class

---
+++ src/main/java/dev/kavu/gameapi/ConditionalListener.java
@@ -0,0 +1,39 @@
+package dev.kavu.gameapi;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+
+import java.lang.reflect.Method;
+import java.util.function.Predicate;
+
+public class ConditionalListener implements Listener {
+
+    // Fields
+    private final Listener listener;
+
+    private final Predicate<Plugin> condition;
+
+    private final Plugin plugin;
+
+    // Constructor
+    public ConditionalListener(Predicate<Plugin> condition, Listener handledListener, Plugin plugin) {
+        this.condition = condition;
+        this.listener = handledListener;
+        this.plugin = plugin;
+    }
+
+    // Functionality
+    public final void onEvent(Event event){
+        for(Method method : listener.getClass().getDeclaredMethods()){
+
+            if(method.getParameterCount() != 1) return;
+            if(!condition.test(plugin)) return;
+
+            try {
+                method.invoke(listener, event);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+}
