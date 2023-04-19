30fc6cace8b43b5ce5025cde70122a77b5ff3182
TheKaVu <k.wieczaszek@gmail.com>
2023-04-19 16:06:04 +0200

    Renamed listener field to handledListener

--- src/main/java/dev/kavu/gameapi/ConditionalListener.java
+++ src/main/java/dev/kavu/gameapi/ConditionalListener.java
@@ -10,31 +10,31 @@ import java.util.function.BooleanSupplier;
 public class ConditionalListener implements Listener {
 
     // Fields
-    private final Listener listener;
+    private final Listener handledListener;
 
     private final BooleanSupplier condition;
 
     // Constructor
     public ConditionalListener(BooleanSupplier condition, Listener handledListener) {
         this.condition = condition;
-        this.listener = handledListener;
+        this.handledListener = handledListener;
     }
 
     // Getters
-    public Listener getListener() {
-        return listener;
+    public Listener getHandledListener() {
+        return handledListener;
     }
 
     // Functionality
     @EventHandler
     public final void onEvent(Event event){
-        for(Method method : listener.getClass().getDeclaredMethods()){
+        for(Method method : handledListener.getClass().getDeclaredMethods()){
 
             if(method.getParameterCount() != 1) return;
             if(!condition.getAsBoolean()) return;
 
             try {
-                method.invoke(listener, event);
+                method.invoke(handledListener, event);
             } catch (Exception ignored) {
             }
         }
