bfb275e90275ec4cdd368b35b0ebb0c8dc87e1fe
TheKaVu <k.wieczaszek@gmail.com>
2023-04-20 17:14:27 +0200

    Added Statistic.java abstract class

---
+++ src/main/java/dev/kavu/gameapi/Statistic.java
@@ -0,0 +1,30 @@
+package dev.kavu.gameapi;
+
+public abstract class Statistic<T extends Number> {
+
+    // Fields
+    private final T defaultValue;
+
+    private boolean locked;
+
+    // Constructor
+    public Statistic(T defaultValue){
+        this.defaultValue = defaultValue;
+        locked = false;
+    }
+
+    // Getters & setters
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public abstract void reset();
+
+    public T getDefaultValue(){
+        return defaultValue;
+    }
+}
