0c2d94cf5a7e6092b9a2ec2c4a261678a94e362c
TheKaVu <k.wieczaszek@gmail.com>
2023-04-29 12:50:07 +0200

    Added Area.Target enumeration specifying block and player rule targeting. Change Area class constructors to get the Area.Target object as parameter

--- src/main/java/dev/kavu/gameapi/Area.java
+++ src/main/java/dev/kavu/gameapi/Area.java
@@ -14,16 +14,18 @@ public class Area {
     private boolean blockPlacement;
     private boolean blockDestruction;
     private boolean blockInteraction;
+    private Target target;
 
     // Constructors
-    public Area(int dx, int dy, int dz, Location originPos) {
+    public Area(int dx, int dy, int dz, Location originPos, Target affectTarget) {
         this.dx = dx;
         this.dy = dy;
         this.dz = dz;
         this.originPos = originPos;
+        target = affectTarget;
     }
 
-    public Area(Location corner1, Location corner2) {
+    public Area(Location corner1, Location corner2, Target affectTarget) {
         dx = Math.abs(corner1.getBlockX() - corner2.getBlockX());
         dy = Math.abs(corner1.getBlockY() - corner2.getBlockY());
         dz = Math.abs(corner1.getBlockZ() - corner2.getBlockZ());
@@ -33,6 +35,8 @@ public class Area {
                 Math.min(corner1.getBlockY(), corner2.getBlockY()),
                 Math.min(corner1.getBlockZ(), corner2.getBlockZ())
         );
+
+        target = affectTarget;
     }
 
     // Getters & Setters
@@ -76,6 +80,14 @@ public class Area {
         this.blockInteraction = blockInteraction;
     }
 
+    public Target getTarget(){
+        return target;
+    }
+
+    public void setTarget(Target target){
+        this.target = target;
+    }
+
     // Functionality
     public Area resize(int xd, int yd, int zd){
         this.dx = xd;
@@ -105,4 +117,27 @@ public class Area {
     public void onLeave(Player player){
 
     }
+
+    public enum Target {
+        BLOCKS_ONLY(false, true),
+        PLAYERS_ONLY(true, false),
+        BLOCKS_AND_PLAYERS(true, true);
+
+        private final boolean affectPlayers;
+        private final boolean affectBlocks;
+
+        Target(boolean affectPlayers, boolean affectBlocks) {
+            this.affectPlayers = affectPlayers;
+            this.affectBlocks = affectBlocks;
+        }
+
+        public boolean affectsPlayers() {
+            return affectPlayers;
+        }
+
+        public boolean affectsBlocks() {
+            return affectBlocks;
+        }
+    }
+
 }
