2887a16a0dfb3575bc25313094698b7166aa95a1
TheKaVu <k.wieczaszek@gmail.com>
2023-05-03 17:36:50 +0200

    Added StatisticSet.java class

---
+++ src/main/java/dev/kavu/gameapi/StatisticSet.java
@@ -0,0 +1,48 @@
+package dev.kavu.gameapi;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.function.BiPredicate;
+
+public abstract class StatisticSet<V> extends Statistic<V> {
+
+    private final HashMap<UUID, Statistic<V>> map = new HashMap<>();
+
+    public StatisticSet(V defaultValue) {
+        super(defaultValue);
+    }
+
+    public StatisticSet(V defaultValue, Collection<UUID> members) {
+        super(defaultValue);
+        for (UUID id : members) {
+            map.put(id, new Statistic<V>(defaultValue) {});
+        }
+    }
+
+    @Override
+    public void set(V value) {
+        map.forEach((k, v) -> v.set(value));
+    }
+
+    public void set(BiPredicate<UUID, V> selector, V value) {
+        map.forEach((k, v) -> {
+            if(selector.test(k, v.get())) v.set(value);
+        });
+    }
+
+    @Override
+    public V get(){
+        return getDefault();
+    }
+
+    public Statistic<V> get(UUID uuid){
+        return map.get(uuid);
+    }
+
+    @Override
+    public void reset(){
+        map.forEach((k, v) -> v.reset());
+    }
+
+}
