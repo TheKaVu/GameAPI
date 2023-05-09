12df33b77a576786c01e25c76f938dacd392c149
TheKaVu <k.wieczaszek@gmail.com>
2023-05-09 11:43:23 +0200

    Added new AreaPickEquivocationException constructor (#15)

--- src/main/java/dev/kavu/gameapi/world/AreaPickEquivocationException.java
+++ src/main/java/dev/kavu/gameapi/world/AreaPickEquivocationException.java
@@ -17,4 +17,8 @@ public class AreaPickEquivocationException extends RuntimeException{
     public AreaPickEquivocationException(String message, Throwable cause){
         super(message, cause);
     }
+
+    public AreaPickEquivocationException(Area area1, Area area2){
+        super("Equivocation between area: " + area1 + " and area:" + area2 + " while picking most prioritized - both have the same priority");
+    }
 }
