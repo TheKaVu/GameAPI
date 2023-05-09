f95e0ef0f3efe7724d8ef8eace7d117b970c1b0d
TheKaVu <k.wieczaszek@gmail.com>
2023-05-09 15:43:27 +0200

    Added NullPointerException throw to constructor and functions in StatisticWrapper class (#15)

--- src/main/java/dev/kavu/gameapi/statistic/StatisticWrapper.java
+++ src/main/java/dev/kavu/gameapi/statistic/StatisticWrapper.java
@@ -15,6 +15,9 @@ public class StatisticWrapper<V extends Serializable> implements Serializable {
     }
 
     public <T extends Statistic<V>> T getStatistic(T newStatistic) {
+        if(newStatistic == null){
+            throw new NullPointerException();
+        }
         newStatistic.setLocked(false);
         newStatistic.set(value);
         newStatistic.setLocked(locked);
@@ -22,6 +25,9 @@ public class StatisticWrapper<V extends Serializable> implements Serializable {
     }
 
     public <T extends Statistic<V>> T getStatistic(Class<T> clazz) throws InstantiationException, InvocationTargetException{
+        if(clazz == null){
+            throw new NullPointerException();
+        }
         try {
             T statistic = clazz.getConstructor().newInstance();
             statistic.setLocked(false);
@@ -34,10 +40,16 @@ public class StatisticWrapper<V extends Serializable> implements Serializable {
     }
 
     public static <V extends Serializable> StatisticWrapper<V> wrap(Statistic<V> statistic) {
+        if(statistic == null){
+            throw new NullPointerException();
+        }
         return new StatisticWrapper<>(statistic.get(), statistic.isLocked());
     }
 
     public static <V extends Serializable> StatisticWrapper<V> wrap(Supplier<Statistic<V>> supplier){
+        if(supplier == null){
+            throw new NullPointerException();
+        }
         return wrap(supplier.get());
     }
 }
