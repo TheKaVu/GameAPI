e4c1152731d9000ebfb052e8824a727e6e7ab5f7
TheKaVu <k.wieczaszek@gmail.com>
2023-05-10 17:12:19 +0200

    Changed AreaPickEquivocationException default message

--- src/main/java/dev/kavu/gameapi/world/AreaPickEquivocationException.java
+++ src/main/java/dev/kavu/gameapi/world/AreaPickEquivocationException.java
@@ -3,22 +3,10 @@ package dev.kavu.gameapi.world;
 public class AreaPickEquivocationException extends RuntimeException{
 
     public AreaPickEquivocationException(){
-        super();
+        super("Equivocation while picking the most prioritized area");
     }
 
     public AreaPickEquivocationException(String message){
         super(message);
-    }
-
-    public AreaPickEquivocationException(Throwable cause){
-        super(cause);
-    }
-
-    public AreaPickEquivocationException(String message, Throwable cause){
-        super(message, cause);
-    }
-
-    public AreaPickEquivocationException(Area area1, Area area2){
-        super("Equivocation between area: " + area1 + " and area:" + area2 + " while picking most prioritized - both have the same priority");
     }
 }
