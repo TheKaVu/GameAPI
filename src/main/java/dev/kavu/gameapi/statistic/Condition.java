856942b3b1ca4e24bce153635ba7d808e3e06d04
TheKaVu <k.wieczaszek@gmail.com>
2023-05-13 20:18:00 +0200

    Added Condition.java class (#19)

---
+++ src/main/java/dev/kavu/gameapi/statistic/Condition.java
@@ -0,0 +1,12 @@
+package dev.kavu.gameapi.statistic;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Condition {
+    // Determines if the function or field should be treated as a condition
+}
