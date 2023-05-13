af2cd8518b51a4804f920cdb37b99927cff921e8
TheKaVu <k.wieczaszek@gmail.com>
2023-05-13 20:41:19 +0200

    Removed Statistic.getValue, Statistic.setValue and Statistic.trigger functions (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -2,13 +2,7 @@ package dev.kavu.gameapi.statistic;
 
 public interface Statistic<T extends Number> {
 
-    T getValue();
-
-    void setValue(T value);
-
     void lock();
 
     boolean isLocked();
-
-    void trigger();
 }
