aa4cccfd666078286ad5273b4d9d6bdcd520c605
TheKaVu <k.wieczaszek@gmail.com>
2023-05-15 17:43:45 +0200

    Removed Statistic.setLocked and Statistic.isLocked functions for new LockableStatistic.java class (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -4,10 +4,6 @@ public interface Statistic<T extends Number> {
 
     T getDefault();
 
-    void setLocked(boolean locked);
-
-    boolean isLocked();
-
     T onTrigger(T current);
 
     Trigger<?>[] getTriggers();
