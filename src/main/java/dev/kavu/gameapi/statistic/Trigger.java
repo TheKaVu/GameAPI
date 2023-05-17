fc0427ae83c2a2c15a043de007e6c3016947bf58
TheKaVu <k.wieczaszek@gmail.com>
2023-05-17 17:54:47 +0200

    Added event validation system using Predicate in Trigger (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Trigger.java
+++ src/main/java/dev/kavu/gameapi/statistic/Trigger.java
@@ -4,15 +4,24 @@ import org.bukkit.event.Event;
 
 import java.util.UUID;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Trigger<E extends Event> {
 
     private final Class<E> eventClass;
     private final Function<E, UUID> mapper;
+    private final Predicate<E> validator;
 
     public Trigger(Class<E> eventClass, Function<E, UUID> mapper) {
         this.eventClass = eventClass;
         this.mapper = mapper;
+        validator = event -> true;
+    }
+
+    public Trigger(Class<E> eventClass, Function<E, UUID> mapper, Predicate<E> validator) {
+        this.eventClass = eventClass;
+        this.mapper = mapper;
+        this.validator = validator;
     }
 
     public Class<E> getEventClass(){
@@ -21,6 +30,10 @@ public class Trigger<E extends Event> {
 
     public UUID compute(Event event){
         return mapper.apply(eventClass.cast(event));
+    }
+
+    public boolean validate(E event){
+        return validator.test(event);
     }
 
     public boolean match(Class<? extends Event> clazz){
