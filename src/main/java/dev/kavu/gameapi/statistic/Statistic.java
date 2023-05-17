4118b5985ed4d83c15011eea28e8fcfb87d85019
TheKaVu <k.wieczaszek@gmail.com>
2023-05-18 00:02:52 +0200

    Added Statistic.getNewRegistry function to easily create matching StatisticRegistry objects (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -1,8 +1,17 @@
 package dev.kavu.gameapi.statistic;
 
+import org.bukkit.plugin.Plugin;
+
+import java.util.Collection;
+import java.util.UUID;
+
 public interface Statistic<T extends Number> {
 
     T getDefault();
 
     Trigger<?>[] getTriggers();
+
+    StatisticRegistry<T> getNewRegistry(Plugin plugin);
+
+    StatisticRegistry<T> getNewRegistry(Collection<UUID> initialMembers, Plugin plugin);
 }
