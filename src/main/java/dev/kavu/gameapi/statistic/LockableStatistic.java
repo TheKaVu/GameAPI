b5e850ef9eac8fec1fb43493c10b2e02d1565593
TheKaVu <k.wieczaszek@gmail.com>
2023-05-18 19:33:30 +0200

    Removed redundant LockableStatistic.getNewRegistry function overrides (#19)

--- src/main/java/dev/kavu/gameapi/statistic/LockableStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/LockableStatistic.java
@@ -1,10 +1,5 @@
 package dev.kavu.gameapi.statistic;
 
-import org.bukkit.plugin.Plugin;
-
-import java.util.Collection;
-import java.util.UUID;
-
 public abstract class LockableStatistic<T extends Number> implements Statistic<T>{
 
     private boolean locked;
@@ -16,15 +11,5 @@ public abstract class LockableStatistic<T extends Number> implements Statistic<T
     @Condition(negate = true)
     public boolean isLocked() {
         return locked;
-    }
-
-    @Override
-    public StatisticRegistry<T> getNewRegistry(Plugin plugin){
-        return new StatisticRegistry<>(this, plugin);
-    }
-
-    @Override
-    public StatisticRegistry<T> getNewRegistry(Collection<UUID> initialMembers, Plugin plugin) {
-        return new StatisticRegistry<>(this, initialMembers, plugin);
     }
 }
