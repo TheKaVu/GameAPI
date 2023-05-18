ea694d595741365ea38d645caac4e2016be1b856
TheKaVu <k.wieczaszek@gmail.com>
2023-05-18 19:32:57 +0200

    Removed redundant Statistic.getNewRegistry function (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -1,18 +1,10 @@
 package dev.kavu.gameapi.statistic;
 
-import org.bukkit.plugin.Plugin;
-
-import java.util.Collection;
 import java.util.Set;
-import java.util.UUID;
 
 public interface Statistic<T extends Number> {
 
     T getDefault();
 
     Set<Trigger<?>> getTriggers();
-
-    StatisticRegistry<T> getNewRegistry(Plugin plugin);
-
-    StatisticRegistry<T> getNewRegistry(Collection<UUID> initialMembers, Plugin plugin);
 }
