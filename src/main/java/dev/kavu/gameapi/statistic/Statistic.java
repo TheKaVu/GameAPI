cf2b95c23006eaba7bf85247859bc93fadf82c64
TheKaVu <k.wieczaszek@gmail.com>
2023-05-18 19:16:41 +0200

    Changed Statistic.getTriggers function return type from Trigger<?>[] to Set<Trigger<?>> (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -3,13 +3,14 @@ package dev.kavu.gameapi.statistic;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Collection;
+import java.util.Set;
 import java.util.UUID;
 
 public interface Statistic<T extends Number> {
 
     T getDefault();
 
-    Trigger<?>[] getTriggers();
+    Set<Trigger<?>> getTriggers();
 
     StatisticRegistry<T> getNewRegistry(Plugin plugin);
 
