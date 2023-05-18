cdca194c0ebcd6915366c62a48e303892685b118
TheKaVu <k.wieczaszek@gmail.com>
2023-05-18 19:22:41 +0200

    Added Trigger response mechanic which provides primary response action (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Trigger.java
+++ src/main/java/dev/kavu/gameapi/statistic/Trigger.java
@@ -11,17 +11,34 @@ public class Trigger<E extends Event> {
     private final Class<E> eventClass;
     private final Function<E, UUID> mapper;
     private final Predicate<E> validator;
+    private final Function<? extends Number, ? extends Number> response;
 
     public Trigger(Class<E> eventClass, Function<E, UUID> mapper) {
         this.eventClass = eventClass;
         this.mapper = mapper;
         validator = event -> true;
+        response = n -> n;
     }
 
     public Trigger(Class<E> eventClass, Function<E, UUID> mapper, Predicate<E> validator) {
         this.eventClass = eventClass;
         this.mapper = mapper;
         this.validator = validator;
+        response = n -> n;
+    }
+
+    public Trigger(Class<E> eventClass, Function<E, UUID> mapper, Function<? extends Number, ? extends Number> response) {
+        this.eventClass = eventClass;
+        this.mapper = mapper;
+        this.response = response;
+        validator = e -> true;
+    }
+
+    public Trigger(Class<E> eventClass, Function<E, UUID> mapper, Predicate<E> validator, Function<? extends Number, ? extends Number> response) {
+        this.eventClass = eventClass;
+        this.mapper = mapper;
+        this.validator = validator;
+        this.response = response;
     }
 
     public Class<E> getEventClass(){
@@ -38,5 +55,9 @@ public class Trigger<E extends Event> {
 
     public boolean match(Class<? extends Event> clazz){
         return clazz.equals(eventClass);
+    }
+
+    public Function<? extends Number, ? extends Number> getResponse() {
+        return response;
     }
 }
