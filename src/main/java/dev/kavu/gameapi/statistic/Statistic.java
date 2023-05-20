1b09595b731bb71c115bab31a22db83c060c6774
TheKaVu <k.wieczaszek@gmail.com>
2023-05-20 21:18:26 +0200

    Added incrementing and decrementing functionality to Statistic (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -7,4 +7,54 @@ public interface Statistic<T extends Number> {
     T getDefault();
 
     Set<Trigger<?>> getTriggers();
+
+    // Utility
+
+    static byte increment(byte a){
+        return ++a;
+    }
+
+    static short increment(short a){
+        return ++a;
+    }
+
+    static int increment(int a){
+        return ++a;
+    }
+
+    static long increment(long a){
+        return ++a;
+    }
+
+    static double increment(double a){
+        return ++a;
+    }
+
+    static float increment(float a){
+        return ++a;
+    }
+
+    static byte decrement(byte a){
+        return --a;
+    }
+
+    static short decrement(short a){
+        return --a;
+    }
+
+    static int decrement(int a){
+        return --a;
+    }
+
+    static long decrement(long a){
+        return --a;
+    }
+
+    static double decrement(double a){
+        return --a;
+    }
+
+    static float decrement(float a){
+        return --a;
+    }
 }
