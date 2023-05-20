7380708147065fb1f389eae3a664df497b8b1a23
TheKaVu <k.wieczaszek@gmail.com>
2023-05-20 21:26:35 +0200

    Moved proper static Statistic.increment and Statistic.decrement overloads to FloatStatistic (#19)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -7,14 +7,4 @@ public interface Statistic<T extends Number> {
     T getDefault();
 
     Set<Trigger<?>> getTriggers();
-
-    // Utility
-
-    static float increment(float a){
-        return ++a;
-    }
-
-    static float decrement(float a){
-        return --a;
-    }
 }
--- src/main/java/dev/kavu/gameapi/statistic/common/FloatStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/common/FloatStatistic.java
@@ -3,4 +3,12 @@ package dev.kavu.gameapi.statistic.common;
 import dev.kavu.gameapi.statistic.Statistic;
 
 public interface FloatStatistic extends Statistic<Float> {
+
+    static float increment(float a){
+        return ++a;
+    }
+
+    static float decrement(float a){
+        return --a;
+    }
 }
