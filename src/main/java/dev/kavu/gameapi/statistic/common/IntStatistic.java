f513a06a6b09df0ffe9f199d89e21f7d73f0612a
TheKaVu <k.wieczaszek@gmail.com>
2023-05-20 12:18:27 +0200

    Changed abstract class IntStatistic to interface (#19)

--- src/main/java/dev/kavu/gameapi/statistic/common/IntStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/common/IntStatistic.java
@@ -1,7 +1,6 @@
 package dev.kavu.gameapi.statistic.common;
 
-import dev.kavu.gameapi.statistic.LockableStatistic;
+import dev.kavu.gameapi.statistic.Statistic;
 
-public abstract class IntStatistic extends LockableStatistic<Integer> {
-
+public interface IntStatistic extends Statistic<Integer> {
 }
