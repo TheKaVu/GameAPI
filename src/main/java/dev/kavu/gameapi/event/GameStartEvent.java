f0a7ea3ee102fad39f8ad3eaf71f8e337d32f70a
TheKaVu <k.wieczaszek@gmail.com>
2023-05-23 09:12:28 +0200

    Added GameStartEvent.java class (#30)

---
+++ src/main/java/dev/kavu/gameapi/event/GameStartEvent.java
@@ -0,0 +1,41 @@
+package dev.kavu.gameapi.event;
+
+import dev.kavu.gameapi.game.GameType;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.HandlerList;
+
+public class GameStartEvent extends GameEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final int playerCount;
+    private boolean cancelled;
+
+    public GameStartEvent(GameType gameType, int playerCount) {
+        super(gameType);
+        this.playerCount = playerCount;
+    }
+
+    public int getPlayerCount() {
+        return playerCount;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancelled = cancel;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+}
