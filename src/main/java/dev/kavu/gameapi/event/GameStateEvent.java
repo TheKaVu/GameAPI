b75c81b80557da8b2fa4736e7489f9ad55c0538a
TheKaVu <k.wieczaszek@gmail.com>
2023-05-23 09:06:39 +0200

    Added GameStateEvent.java class (#30)

---
+++ src/main/java/dev/kavu/gameapi/event/GameStateEvent.java
@@ -0,0 +1,36 @@
+package dev.kavu.gameapi.event;
+
+import dev.kavu.gameapi.game.GameState;
+import dev.kavu.gameapi.game.GameType;
+import org.bukkit.event.HandlerList;
+
+public class GameStateEvent extends GameEvent{
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private final GameState gameState;
+    private final boolean scheduled;
+
+    public GameStateEvent(GameType gameType, GameState gameState, boolean scheduled) {
+        super(gameType);
+        this.gameState = gameState;
+        this.scheduled = scheduled;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public boolean isScheduled() {
+        return scheduled;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
+
+    public static HandlerList getHandlerList(){
+        return handlers;
+    }
+}
