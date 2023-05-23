8a8a8de18a3289be2c33768d78d281f31d8f4bd3
TheKaVu <k.wieczaszek@gmail.com>
2023-05-23 09:20:56 +0200

    Changed playerCount to Set of players and renamed to players in GameEndEvent (#30)

--- src/main/java/dev/kavu/gameapi/event/GameEndEvent.java
+++ src/main/java/dev/kavu/gameapi/event/GameEndEvent.java
@@ -1,23 +1,27 @@
 package dev.kavu.gameapi.event;
 
 import dev.kavu.gameapi.game.GameType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
+
+import java.util.Set;
 
 public class GameEndEvent extends GameEvent implements Cancellable {
 
     private static final HandlerList handlers = new HandlerList();
 
-    private final int playerCount;
+
+    private final Set<Player> players;
     private boolean cancelled;
 
-    public GameEndEvent(GameType gameType, int playerCount) {
+    public GameEndEvent(GameType gameType, Set<Player> players) {
         super(gameType);
-        this.playerCount = playerCount;
+        this.players = players;
     }
 
-    public int getPlayerCount() {
-        return playerCount;
+    public Set<Player> getPlayers() {
+        return players;
     }
 
     @Override
