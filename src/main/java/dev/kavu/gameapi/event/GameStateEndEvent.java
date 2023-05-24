137e9b20d1d04fc6d21367cd2cf5e3311da87876
TheKaVu <k.wieczaszek@gmail.com>
2023-05-25 00:02:41 +0200

    Added GameStateEndEvent.natural field to determine if the end was natural or forced (#30)

--- src/main/java/dev/kavu/gameapi/event/GameStateEndEvent.java
+++ src/main/java/dev/kavu/gameapi/event/GameStateEndEvent.java
@@ -10,14 +10,20 @@ public class GameStateEndEvent extends GameStateTimerEvent {
     private static final HandlerList handlers = new HandlerList();
 
     private final GameState nextState;
+    private final boolean natural;
 
-    public GameStateEndEvent(GameStateTimer timer, GameState gameState, GameState nextState) {
+    public GameStateEndEvent(GameStateTimer timer, GameState gameState, GameState nextState, boolean natural) {
         super(timer, gameState);
         this.nextState = nextState;
+        this.natural = natural;
     }
 
     public GameState getNextState() {
         return nextState;
+    }
+
+    public boolean isNatural() {
+        return natural;
     }
 
     @Override
