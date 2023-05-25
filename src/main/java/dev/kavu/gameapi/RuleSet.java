4848960c5fdb2efeecf5cdb2d46b01f466f4b751
TheKaVu <k.wieczaszek@gmail.com>
2023-05-25 19:30:27 +0200

    Added RuleSet.getRule function overload for instant cast (#33)

--- src/main/java/dev/kavu/gameapi/RuleSet.java
+++ src/main/java/dev/kavu/gameapi/RuleSet.java
@@ -3,17 +3,21 @@ package dev.kavu.gameapi;
 import java.util.HashSet;
 import java.util.Objects;
 
-public class RuleSet extends HashSet<Rule> {
+public class RuleSet extends HashSet<Rule<?>> {
 
-    public Rule getRule(String name) {
-        for (Rule rule : this) {
+    public Rule<?> getRule(String name) {
+        for (Rule<?> rule : this) {
             if (Objects.equals(rule.getName(), name)) return rule;
         }
         return null;
     }
 
+    public <E extends Enum<E>> Rule<E> getRule(String name, Class<E> clazz) {
+        return (Rule<E>) getRule(name);
+    }
+
     @Override
-    public boolean add(Rule rule) {
+    public boolean add(Rule<?> rule) {
         if(getRule(rule.getName()) != null) return false;
         return super.add(rule);
     }
