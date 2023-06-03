477d8a5bcf50d2a805be63818c4f8b90086eecc9
TheKaVu <k.wieczaszek@gmail.com>
2023-06-03 13:52:03 +0200

    Added BorderShape.SPHERE constant (#45)

--- src/main/java/dev/kavu/gameapi/world/BorderShape.java
+++ src/main/java/dev/kavu/gameapi/world/BorderShape.java
@@ -1,7 +1,16 @@
 package dev.kavu.gameapi.world;
 
+import javafx.geometry.Point3D;
 import org.bukkit.Location;
 
 public interface BorderShape {
     boolean compute(Border border, Location pos);
+
+    BorderShape SPHERE = (border, pos) -> {
+        Location center = border.getCenter();
+        Point3D centerPoint = new Point3D(center.getX(), center.getY(), center.getZ());
+        Point3D posPoint = new Point3D(pos.getX(), pos.getY(), pos.getZ());
+
+        return centerPoint.distance(posPoint) <= border.getRadius();
+    };
 }
