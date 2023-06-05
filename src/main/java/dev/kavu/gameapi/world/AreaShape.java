356774a450d73ad0973536b095b53976efd5842e
TheKaVu <k.wieczaszek@gmail.com>
2023-06-05 18:47:03 +0200

    Renamed BorderShape.java to AreaShape.java (#39)

--- src/main/java/dev/kavu/gameapi/world/AreaShape.java
+++ src/main/java/dev/kavu/gameapi/world/AreaShape.java
@@ -4,10 +4,10 @@ import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
 import org.bukkit.Location;
 
-public interface BorderShape {
+public interface AreaShape {
     boolean compute(Border border, Location pos);
 
-    BorderShape SPHERE = (border, pos) -> {
+    AreaShape SPHERE = (border, pos) -> {
         Location center = border.getCenter();
         Point3D centerPoint = new Point3D(center.getX(), center.getY(), center.getZ());
         Point3D posPoint = new Point3D(pos.getX(), pos.getY(), pos.getZ());
@@ -15,7 +15,7 @@ public interface BorderShape {
         return centerPoint.distance(posPoint) <= border.getRadius();
     };
 
-    BorderShape CYLINDER = (border, pos) -> {
+    AreaShape CYLINDER = (border, pos) -> {
         Location center = border.getCenter();
         Point2D centerPoint = new Point2D(center.getX(), center.getZ());
         Point2D posPoint = new Point2D(pos.getX(), pos.getZ());
@@ -23,7 +23,7 @@ public interface BorderShape {
         return centerPoint.distance(posPoint) <= border.getRadius() && Math.abs(center.getY() - pos.getY()) <= border.getHeight();
     };
 
-    BorderShape RECTANGLE = (border, pos) -> {
+    AreaShape RECTANGLE = (border, pos) -> {
         Location center = border.getCenter();
         Point3D centerPoint = new Point3D(center.getX(), center.getY(), center.getZ());
         Point3D posPoint = new Point3D(pos.getX(), pos.getY(), pos.getZ());
