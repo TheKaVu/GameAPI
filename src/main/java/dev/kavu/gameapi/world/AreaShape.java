bd4c82ca3ce22d835421cc4d31047756bbb94b2c
TheKaVu <k.wieczaszek@gmail.com>
2023-06-05 20:24:01 +0200

    Remade AreaShape (#32)

--- src/main/java/dev/kavu/gameapi/world/AreaShape.java
+++ src/main/java/dev/kavu/gameapi/world/AreaShape.java
@@ -5,29 +5,29 @@ import javafx.geometry.Point3D;
 import org.bukkit.Location;
 
 public interface AreaShape {
-    boolean compute(Border border, Location pos);
+    boolean compute(Area area, Location pos);
 
-    AreaShape SPHERE = (border, pos) -> {
-        Location center = border.getCenter();
+    AreaShape SPHERE = (area, pos) -> {
+        Location center = area.getCenter();
         Point3D centerPoint = new Point3D(center.getX(), center.getY(), center.getZ());
         Point3D posPoint = new Point3D(pos.getX(), pos.getY(), pos.getZ());
 
-        return centerPoint.distance(posPoint) <= border.getRadius();
+        return (posPoint.getX() - centerPoint.getX()) / area.getSizeX() + (posPoint.getY() - centerPoint.getY()) / area.getSizeY() + (posPoint.getZ() - centerPoint.getZ()) / area.getSizeZ() <= 1;
     };
 
-    AreaShape CYLINDER = (border, pos) -> {
-        Location center = border.getCenter();
+    AreaShape CYLINDER= (area, pos) -> {
+        Location center = area.getCenter();
         Point2D centerPoint = new Point2D(center.getX(), center.getZ());
         Point2D posPoint = new Point2D(pos.getX(), pos.getZ());
 
-        return centerPoint.distance(posPoint) <= border.getRadius() && Math.abs(center.getY() - pos.getY()) <= border.getHeight();
+        return (posPoint.getX() - centerPoint.getX()) / area.getSizeX() + (posPoint.getY() - centerPoint.getY()) / area.getSizeY() <= 1 && Math.abs(center.getY() - pos.getY()) <= area.getSizeZ();
     };
 
-    AreaShape RECTANGLE = (border, pos) -> {
-        Location center = border.getCenter();
+    AreaShape RECTANGLE = (area, pos) -> {
+        Location center = area.getCenter();
         Point3D centerPoint = new Point3D(center.getX(), center.getY(), center.getZ());
         Point3D posPoint = new Point3D(pos.getX(), pos.getY(), pos.getZ());
         Point3D diff = centerPoint.subtract(posPoint);
-        return Math.abs(diff.getX()) <= border.getRadius() && Math.abs(diff.getZ()) <= border.getRadius() && Math.abs(diff.getY()) <= border.getHeight();
+        return Math.abs(diff.getX()) <= area.getSizeX() && Math.abs(diff.getY()) <= area.getSizeY() && Math.abs(diff.getZ()) <= area.getSizeZ();
     };
 }
