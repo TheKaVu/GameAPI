e9d503eb6582a636df17c4ff33389eadc8d8855a
TheKaVu <k.wieczaszek@gmail.com>
2023-06-08 21:58:37 +0200

    Added new PlainArea constructor to match the Area constructor

--- src/main/java/dev/kavu/gameapi/world/PlainArea.java
+++ src/main/java/dev/kavu/gameapi/world/PlainArea.java
@@ -3,7 +3,13 @@ package dev.kavu.gameapi.world;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
+import java.util.function.Supplier;
+
 public class PlainArea extends Area{
+
+    public PlainArea(Supplier<Location> center, double x, double y, double z, AreaShape shape) {
+        super(center, x, y, z, shape);
+    }
 
     public PlainArea(Location center, double x, double y, double z, AreaShape shape) {
         super(center, x, y, z, shape);
