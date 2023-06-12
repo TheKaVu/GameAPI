0419d6b2d154f07c0dd3c5e2b3146a3abd30c22f
TheKaVu <k.wieczaszek@gmail.com>
2023-06-12 19:55:43 +0200

    Changed GameStateInterruptException default message (#53)

--- src/main/java/dev/kavu/gameapi/GameStateInterruptException.java
+++ src/main/java/dev/kavu/gameapi/GameStateInterruptException.java
@@ -3,7 +3,7 @@ package dev.kavu.gameapi;
 public class GameStateInterruptException extends Exception {
 
     public GameStateInterruptException() {
-        super("Tried to initialize a new game state over an uninterruptible state");
+        super("Tried to initialize a new game state over an uninterruptible one");
     }
 
     public GameStateInterruptException(String message) {
