2f920c069e6e0f47d726423b3679761bf26d9805
TheKaVu <k.wieczaszek@gmail.com>
2023-06-16 22:42:54 +0200

    Added primary category reference to Game class (#55)

--- src/main/java/dev/kavu/gameapi/Game.java
+++ src/main/java/dev/kavu/gameapi/Game.java
@@ -4,7 +4,7 @@ import org.apache.commons.lang.Validate;
 
 import java.util.Properties;
 
-public class Game {
+public abstract class Game {
 
     // Fields
     private final String name;
@@ -19,12 +19,14 @@ public class Game {
 
     private final Properties properties;
 
+    private final Class<? extends Category<Game>> category;
+
     // Constructors
-    public Game(String name, String systemName, String prefix, int minPlayers, int maxPlayers) {
-        this(name, systemName, prefix, minPlayers, maxPlayers, new Properties());
+    public Game(String name, String systemName, String prefix, int minPlayers, int maxPlayers, Class<? extends Category<Game>> category) {
+        this(name, systemName, prefix, minPlayers, maxPlayers, new Properties(), category);
     }
 
-    public Game(String name, String systemName, String prefix, int minPlayers, int maxPlayers, Properties properties) {
+    public Game(String name, String systemName, String prefix, int minPlayers, int maxPlayers, Properties properties, Class<? extends Category<Game>> category) {
         Validate.notNull(name, "name cannot be null");
         Validate.notNull(systemName, "systemName cannot be null");
         Validate.notNull(prefix, "prefix cannot be null");
@@ -38,6 +40,7 @@ public class Game {
         this.properties = properties;
         properties.setProperty("min_players", String.valueOf(minPlayers));
         properties.setProperty("max_players", String.valueOf(maxPlayers));
+        this.category = category;
     }
 
     // Getters
@@ -63,6 +66,10 @@ public class Game {
 
     public Properties getProperties() {
         return properties;
+    }
+
+    public Class<? extends Category<Game>> getCategory() {
+        return category;
     }
 
     // Overrides
