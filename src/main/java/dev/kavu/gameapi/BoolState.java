46c6d7d7651bf62dc19736c164ffa30e1988f689
TheKaVu <k.wieczaszek@gmail.com>
2023-06-26 23:30:41 +0200

    Added Java comment documentation for BoolState enum (#16)

--- src/main/java/dev/kavu/gameapi/BoolState.java
+++ src/main/java/dev/kavu/gameapi/BoolState.java
@@ -1,8 +1,19 @@
 package dev.kavu.gameapi;
 
+/**
+ * An enum representing {@code boolean} value.
+ */
 public enum BoolState {
-    TRUE (true),
-    FALSE (false);
+
+    /**
+     * Represents {@code false} value
+     */
+    FALSE (false),
+
+    /**
+     * Represents {@code true} value
+     */
+    TRUE (true);
 
     private final boolean value;
 
@@ -10,11 +21,17 @@ public enum BoolState {
         this.value = value;
     }
 
-    public boolean isTrue() {
-        return value;
-    }
-
+    /**
+     * @return {@code true} if equal to {@link #FALSE}, {@code false} otherwise
+     */
     public boolean isFalse() {
         return !value;
+    }
+
+    /**
+     * @return {@code true} if equal to {@link #TRUE}, {@code false} otherwise
+     */
+    public boolean isTrue() {
+        return value;
     }
 }
