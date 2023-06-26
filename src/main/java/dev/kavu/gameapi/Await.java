4d1550f6c7a72cb353786100414d28028553dd59
TheKaVu <k.wieczaszek@gmail.com>
2023-06-26 22:52:22 +0200

    Added Java comment documentation for Await class (#16)

--- src/main/java/dev/kavu/gameapi/Await.java
+++ src/main/java/dev/kavu/gameapi/Await.java
@@ -1,6 +1,14 @@
 package dev.kavu.gameapi;
 
+/**
+ * Subclass of {@link GameState} class used to create a time gap between separate states. As this class is in fact a <tt>GameState</tt>, it can be also added to the schedule.
+ */
 public final class Await extends GameState{
+
+    /**
+     * Creates new instance of <tt>Await</tt> class with specific duration.
+     * @param duration Duration of this state expressed in milliseconds
+     */
     public Await(long duration) {
         super("await", duration, false);
     }
