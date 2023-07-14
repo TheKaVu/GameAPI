f9463cb789e276b90a4ef12497b0c54ded8abf20
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 16:05:08 +0200

    Added Java comment documentation for Trigger class (#16)

--- src/main/java/dev/kavu/gameapi/statistic/Condition.java
+++ src/main/java/dev/kavu/gameapi/statistic/Condition.java
@@ -5,11 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Classifies the method as a condition/
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Condition {
     // Determines if the function or field should be treated as a condition
 
+    /**
+     * @return {@code true} if the result of condition should be negated
+     */
     boolean negate() default false;
+
+    /**
+     * Determines if condition should be treated as an alternative one. If one of the non-alternative conditions fails, any passed alternative condition will make the whole test pass.
+     * @return {@code true} if the condition should be treated as alternative
+     */
     boolean alternative() default false;
 }
