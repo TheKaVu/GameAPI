276d7b27b9af00c8172995faaa7d51ed6b3a9c3c
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 14:00:02 +0200

    Added Java comment documentation for ShortStatistic interface (#16)

--- src/main/java/dev/kavu/gameapi/statistic/common/ShortStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/common/ShortStatistic.java
@@ -2,12 +2,25 @@ package dev.kavu.gameapi.statistic.common;
 
 import dev.kavu.gameapi.statistic.Statistic;
 
+/**
+ * Subinterface of {@link Statistic} interface of <tt>short</tt> type
+ */
 public interface ShortStatistic extends Statistic<Short> {
 
+    /**
+     * Incrementor for <tt>short</tt> numbers
+     * @param a Number to be incremented
+     * @return Incremented number
+     */
     static short increment(short a){
         return ++a;
     }
 
+    /**
+     * Decrementor for <tt>short</tt> numbers
+     * @param a Number to be decremented
+     * @return Decremented number
+     */
     static short decrement(short a){
         return --a;
     }
