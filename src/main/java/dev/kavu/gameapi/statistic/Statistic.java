ac4b9518580ab441c1799bd5aef8a90456f0d0d1
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 14:11:15 +0200

    Updated Java comment documentation for Statistic interface (#16)

--- src/main/java/dev/kavu/gameapi/statistic/Statistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/Statistic.java
@@ -4,7 +4,7 @@ import java.util.Set;
 
 /**
  * This interface is the base for any statistic. It classifies the triggers this statistic will be counted with as well as its default value. <br/>
- * There are predefined subinterfaces of this interface for every numeric type in {@link dev.kavu.gameapi.statistic.common .common} package. <br/><br/>
+ * There are predefined subinterfaces of this interface for every numeric type in {@link dev.kavu.gameapi.statistic.common .common} package. <p/>
  * <b>Note: </b> For statistic to work it has to be registered using {@link dev.kavu.gameapi.GameManager GameManager}.
  *
  * @param <T> Numeric type of this statistic
