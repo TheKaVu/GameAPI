78d21c3e73f36d5e9d770c6d70fdccd35497f8e9
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 14:02:15 +0200

    Added Java comment documentation for DoubleStatistic interface (#16)

--- src/main/java/dev/kavu/gameapi/statistic/common/DoubleStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/common/DoubleStatistic.java
@@ -2,12 +2,25 @@ package dev.kavu.gameapi.statistic.common;
 
 import dev.kavu.gameapi.statistic.Statistic;
 
+/**
+ * Subinterface of {@link Statistic} interface of <tt>double</tt> type
+ */
 public interface DoubleStatistic extends Statistic<Double> {
 
+    /**
+     * Incrementor for <tt>double</tt> numbers
+     * @param a Number to be incremented
+     * @return Incremented number
+     */
     static double increment(double a){
         return ++a;
     }
 
+    /**
+     * Decrementor for <tt>double</tt> numbers
+     * @param a Number to be decremented
+     * @return Decremented number
+     */
     static double decrement(double a){
         return --a;
     }
