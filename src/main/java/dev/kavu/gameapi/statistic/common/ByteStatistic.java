bb27d06d32ea071b2d7d4154d1575b747f6938ac
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 13:58:11 +0200

    Added Java comment documentation for ByteStatistic interface (#16)

--- src/main/java/dev/kavu/gameapi/statistic/common/ByteStatistic.java
+++ src/main/java/dev/kavu/gameapi/statistic/common/ByteStatistic.java
@@ -2,12 +2,25 @@ package dev.kavu.gameapi.statistic.common;
 
 import dev.kavu.gameapi.statistic.Statistic;
 
+/**
+ * Subinterface of {@link Statistic} interface of {@code byte} type
+ */
 public interface ByteStatistic extends Statistic<Byte> {
 
+    /**
+     * Incrementor for {@code byte} numbers
+     * @param a Number to be incremented
+     * @return Incremented number
+     */
     static byte increment(byte a){
         return ++a;
     }
 
+    /**
+     * Decrementor for {@code byte} numbers
+     * @param a Number to be decremented
+     * @return Decremented number
+     */
     static byte decrement(byte a){
         return --a;
     }
