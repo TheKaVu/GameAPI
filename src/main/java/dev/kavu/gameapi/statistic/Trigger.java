5e710048fbb7b245789796af2a75f5f2b9f60e56
TheKaVu <k.wieczaszek@gmail.com>
2023-07-14 16:01:25 +0200

    Added Java comment documentation for Trigger class (#16)

--- src/main/java/dev/kavu/gameapi/statistic/Trigger.java
+++ src/main/java/dev/kavu/gameapi/statistic/Trigger.java
@@ -7,6 +7,10 @@ import java.util.UUID;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
+/**
+ * Representation of automatic statistic trigger. It is used to detect event calls and perform specific action on statistics dependently on which one is called.
+ * @param <E> Event to be caught if called
+ */
 public class Trigger<E extends Event> {
 
     private final Class<E> eventClass;
@@ -14,6 +18,11 @@ public class Trigger<E extends Event> {
     private final Predicate<E> validator;
     private final Function<? extends Number, ? extends Number> response;
 
+    /**
+     * Creates new instance of <tt>Trigger</tt> class.
+     * @param eventClass Event to be caught
+     * @param mapper Mapping function retrieving {@link UUID} object form {@link E} object, which will represent statistic member
+     */
     public Trigger(Class<E> eventClass, Function<E, UUID> mapper) {
         Validate.notNull(eventClass, "eventClass cannot be null");
         Validate.notNull(mapper, "mapper cannot be null");
@@ -24,6 +33,12 @@ public class Trigger<E extends Event> {
         response = n -> n;
     }
 
+    /**
+     * Creates new instance of <tt>Trigger</tt> class with event validator.
+     * @param eventClass Event to be caught
+     * @param mapper Mapping function retrieving {@link UUID} object form {@link E} object, which will represent statistic member
+     * @param validator Validating function which tests the caught event; trigger will be run if returns {@code true}
+     */
     public Trigger(Class<E> eventClass, Function<E, UUID> mapper, Predicate<E> validator) {
         Validate.notNull(eventClass, "eventClass cannot be null");
         Validate.notNull(mapper, "mapper cannot be null");
@@ -35,6 +50,12 @@ public class Trigger<E extends Event> {
         response = n -> n;
     }
 
+    /**
+     * Creates new instance of <tt>Trigger</tt> class with event response.
+     * @param eventClass Event to be caught
+     * @param mapper Mapping function retrieving {@link UUID} object form {@link E} object, which will represent statistic member
+     * @param response Function to be executed on member retrieved for event using mapper
+     */
     public <T extends Number> Trigger(Class<E> eventClass, Function<E, UUID> mapper, Function<T, T> response) {
         Validate.notNull(eventClass, "eventClass cannot be null");
         Validate.notNull(mapper, "mapper cannot be null");
@@ -46,6 +67,13 @@ public class Trigger<E extends Event> {
         validator = e -> true;
     }
 
+    /**
+     * Creates new instance of <tt>Trigger</tt> class with event validator and response.
+     * @param eventClass Event to be caught
+     * @param mapper Mapping function retrieving {@link UUID} object form {@link E} object, which will represent statistic member
+     * @param validator Validating function which tests the caught event; trigger will be run if returns {@code true}
+     * @param response Function to be executed on member retrieved for event using mapper
+     */
     public <T extends Number> Trigger(Class<E> eventClass, Function<E, UUID> mapper, Predicate<E> validator, Function<T, T> response) {
         Validate.notNull(eventClass, "eventClass cannot be null");
         Validate.notNull(mapper, "mapper cannot be null");
@@ -58,22 +86,38 @@ public class Trigger<E extends Event> {
         this.response = response;
     }
 
+    /**
+     * @return Monitored event's class
+     */
     public Class<E> getEventClass(){
         return eventClass;
     }
 
+    /**
+     * Retrieves {@link UUID} object from event using mapper, representing a member
+     * @param event Incoming event
+     * @return {@link UUID} representation of member
+     */
     public UUID compute(Event event){
         Validate.notNull(event, "event cannot be null");
 
         return mapper.apply(eventClass.cast(event));
     }
 
+    /**
+     * Validates event with validator, if set.
+     * @param event Incoming event
+     * @return {@link true} if event passed the test, {@link false} otherwise
+     */
     public boolean validate(Event event){
         Validate.notNull(event, "event cannot be null");
 
         return validator.test(eventClass.cast(event));
     }
 
+    /**
+     * @return Function to be performed on specific member
+     */
     public <T extends Number> Function<T, T> getResponse() {
         return (Function<T, T>) response;
     }
