b0437e4508f0809239ee113ecc3f8448e41e84c0
TheKaVu <k.wieczaszek@gmail.com>
2023-07-16 16:54:20 +0200

    Added Java comment documentation for PlainArea class (#16)

--- src/main/java/dev/kavu/gameapi/world/PlainArea.java
+++ src/main/java/dev/kavu/gameapi/world/PlainArea.java
@@ -5,12 +5,37 @@ import org.bukkit.entity.Player;
 
 import java.util.function.Supplier;
 
-public class PlainArea extends Area{
+/**
+ * Simple area with no enter, move and leave events. In other cases it behaves in the same way any other area does.
+ *
+ * @see Area
+ */
+public class PlainArea extends Area {
 
+    /**
+     * Creates new instance of <tt>PlainArea</tt> in the same way as in {@link Area} class.
+     * @param center Center of the area
+     * @param x Size of the area on X axis
+     * @param y Size of the area on Y axis
+     * @param z Size of the area on Z axis
+     * @param shape Shape of the area represented by {@link AreaShape} object
+     *
+     * @see Area#Area(Supplier, double, double, double, AreaShape)
+     */
     public PlainArea(Supplier<Location> center, double x, double y, double z, AreaShape shape) {
         super(center, x, y, z, shape);
     }
 
+    /**
+     * Creates new instance of <tt>PlainArea</tt> in the same way as in {@link Area} class.
+     * @param center Center of the area
+     * @param x Size of the area on X axis
+     * @param y Size of the area on Y axis
+     * @param z Size of the area on Z axis
+     * @param shape Shape of the area represented by {@link AreaShape} object
+     *
+     * @see Area#Area(Location, double, double, double, AreaShape)
+     */
     public PlainArea(Location center, double x, double y, double z, AreaShape shape) {
         super(center, x, y, z, shape);
     }
