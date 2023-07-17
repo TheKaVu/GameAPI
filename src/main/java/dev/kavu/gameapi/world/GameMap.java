f86e1f66a25ff93ffba0c13e7308a15f491e7093
TheKaVu <k.wieczaszek@gmail.com>
2023-07-17 12:22:01 +0200

    Added Java comment documentation for GameMap interface (#16)

--- src/main/java/dev/kavu/gameapi/world/GameMap.java
+++ src/main/java/dev/kavu/gameapi/world/GameMap.java
@@ -4,11 +4,24 @@ import org.bukkit.World;
 
 import java.io.File;
 
+/**
+ * Representation of minecraft game map.
+ */
 public interface GameMap {
 
+    /**
+     * Called when map is loaded.
+     * @param world Map world
+     */
     void onLoad(World world);
 
+    /**
+     * @return Name of the map
+     */
     String getName();
 
+    /**
+     * @return Source directory origin map is located in
+     */
     File getSource();
 }
