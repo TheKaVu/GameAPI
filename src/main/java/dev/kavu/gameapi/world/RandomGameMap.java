8954b508a56a6107d4d56da36a633427cb42d08c
TheKaVu <k.wieczaszek@gmail.com>
2023-07-29 22:27:16 +0200

    Added Java comment documentation for RandomGameMap class (#16)

--- src/main/java/dev/kavu/gameapi/world/RandomGameMap.java
+++ src/main/java/dev/kavu/gameapi/world/RandomGameMap.java
@@ -5,12 +5,23 @@ import org.apache.commons.lang.Validate;
 import java.io.File;
 import java.util.Random;
 
+/**
+ * Represents a map randomizer being picked map itself.
+ *
+ * @see GameMap
+ * @see LocalGameMap
+ */
 public class RandomGameMap implements GameMap, Cloneable{
 
     private final File source;
     private File mapFolder;
     private String name;
 
+    /**
+     * Creates randomizer that will pick map folders from the source directory
+     * @param source Directory to pick map folders from
+     * @throws MapCreationException When map cannot be created due to invalid data
+     */
     public RandomGameMap(File source) throws MapCreationException {
         Validate.notNull(source, "sourceFolder cannot be null");
         if(source.exists()) throw new MapCreationException("Following directory does not exist: " + source.getAbsolutePath());
@@ -30,10 +41,17 @@ public class RandomGameMap implements GameMap, Cloneable{
         return mapFolder;
     }
 
+    /**
+     * @return Directory maps are being picked from
+     */
     public File getSource() {
         return source;
     }
 
+    /**
+     * Picks random map folder inside source directory and makes itself a map representation
+     * @throws MapCreationException When map cannot be created due to invalid data
+     */
     public void randomize() throws MapCreationException {
         File[] mapFiles = source.listFiles((f ,s) -> f.isDirectory());
         if (mapFiles.length == 0) throw new MapCreationException("Following directory is empty: " + source.getAbsolutePath());
