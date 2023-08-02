6315c98f4c7ad926ae29074963f3d0035e696486
TheKaVu <k.wieczaszek@gmail.com>
2023-08-02 19:53:34 +0200

    Updated Java comment documentation for StatisticRegistry class (#74)

--- src/main/java/dev/kavu/gameapi/statistic/StatisticRegistry.java
+++ src/main/java/dev/kavu/gameapi/statistic/StatisticRegistry.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.UUID;
 
+/**
+ * This class stores all statistics for the game.
+ */
 public class StatisticRegistry {
 
     private final Plugin plugin;
